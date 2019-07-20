20352914ab433413a84dc0fc00a4ff2c52a302ca
Sandy <sangzicao@gmail.com>
2019-07-20 14:26:09 -0700

    add a routes file to store app routes

---
+++ routes.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const router = new express.Router();
+
+/** Homepage: show login/signup or homepage
+ * depending on if user is logged in
+ */
+
+router.get('/', function(req, res, next) {
+  return res.send('hello world');
+});
+
+/** Form to login */
+
+router.get('/login', function(req, res, next) {
+  return res.send('hello world');
+});
+
+/** Handle logging in of user */
+
+router.post('/login', function(req, res, next) {
+  return res.send('hello world');
+});
+
+/** Form to signup */
+
+router.get('/signup', function(req, res, next) {
+  return res.send('hello world');
+});
+
+/** Handle signing up a new user */
+
+router.post('/portfolio', function(req, res, next) {
+  return res.send('hello world');
+});
+
+/** Display user's portfolio with current values */
+
+router.get('/portfolio', function(req, res, next) {
+  return res.send('hello world');
+});
+
+/** Display a list of all the users transactions
+ * only if the user is signed in
+ */
+
+router.get('/transactions', function(req, res, next) {
+  return res.send('hello world');
+});
+
+module.exports = router;
