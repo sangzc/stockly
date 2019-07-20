ea98b1c3df79f8763852edcb416b79adcd126ce7
Sandy <sangzicao@gmail.com>
2019-07-20 15:56:02 -0700

    minor changes

--- server.js
+++ server.js
@@ -1,23 +1,5 @@
 const express = require('express');
-
-const app = express();
-
-
-app.get('/login', function(req, res) {
-  return res.send('hello world');
-});
-
-app.get('/signup', function(req, res) {
-  return res.send('hello world');
-});
-
-app.get('/portfolio', function(req, res) {
-  return res.send('hello world');
-});
-
-app.get('/transactions', function(req, res) {
-  return res.send('hello world');
-});
+const app = require('./app');
 
 app.listen(3000, function(){
   console.log('App on port 3000');
