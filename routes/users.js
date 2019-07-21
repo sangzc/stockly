54a774bbee4d0df2706ac55e0e56f3cca0ca7d1c
Sandy <sangzicao@gmail.com>
2019-07-20 20:41:13 -0700

    add get route for /users/:id

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user');
+
+/** GET / => {users: [user, ...]} */
+
+router.get('/:id', async function(req, res, next) {
+  try {
+    const users = await User.getById(req.params.username);
+    return res.json({user});
+  }
+
+  catch (err) {
+    return next(err);
+  }
+});
+
